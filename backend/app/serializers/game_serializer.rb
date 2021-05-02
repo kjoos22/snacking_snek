14c303aa003745b542d1bf9a232ccb96e19872a1
kjoos22 <kjoos22@gmail.com>
2021-05-02 18:14:53 -0400

    updated player in game serializer

--- backend/app/serializers/game_serializer.rb
+++ backend/app/serializers/game_serializer.rb
@@ -1,4 +1,8 @@
 class GameSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :score, :difficulty, :created_at, :player
+  attributes :score, :difficulty, :created_at
+
+  attribute :player do |object|
+    "#{object.player.name}"
+  end
 end
